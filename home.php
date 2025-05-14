1a8860af9fea6e78bc38b1e40f8b4d18d6b62e04
sosina14 <sosiayu14@gmail.com>
2025-05-14 13:30:15 +0300

    update

--- home.php
+++ home.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+    header("Location: index.html");
+    exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
